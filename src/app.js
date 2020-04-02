64b68379e41ce8ae8dbcc32e91d70f8449e9bd10
Simon Reinisch <simon@reinisch.io>
2020-04-02 20:41:29 +0200

    Add coverage tests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-const btn = document.createElement('button');
-let counter = -1;
-
-btn.addEventListener('click', () => {
-    counter++;
-    btn.innerHTML = `Clicked ${String(counter).padStart(3, '0')} times :)`;
-});
-
-btn.click();
-document.body.appendChild(btn);
+module.exports.add = (a, b) => {
+    return a + b;
+};
